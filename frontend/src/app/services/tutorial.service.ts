5bf3eb47dd470aef00c89885d62c406772e00b91
Raghu504 <peddireddyraghuvardhanreddy@gmail.com>
2025-11-28 17:53:53 +0530

    Update base URL for tutorial API

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://43.204.108.4:8080/api/tutorials';
+const baseUrl = 'http://3.110.119.106:8080/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
